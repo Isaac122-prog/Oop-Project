f60d30614f2d55f2f0ce8c11a274be060d7d9601
Isaac <a1901982@student.adelaide.edu.au>
2025-05-21 11:21:00 +0930

    Player class

--- Player.cpp
+++ Player.cpp
@@ -1,13 +1,50 @@
 #include "Player.h"
-#include "Cafe.h"
 
 #include <iostream>
 
 using namespace std;
 
-Player::Player(){}
-
-void Player::assignTask(){
-    // i don't know how to do this
+Player::Player(){
+    name = "Player";
+    energy = 100;
+    maxEnergy = 100;
 }
 
+Player::Player(const std::string& name, int maxEnergy){
+    name(name), energy(maxEnergy), maxEnergy(maxEnergy) {}
+}
+
+void Player::assignTask(const std::string& task) {
+    tasks.push_back(task);
+}
+
+void Player::performTask() {
+    if (!tasks.empty() && energy >= 10) {
+        std::cout << name << " is performing: " << tasks[0] << "\n";
+        tasks.erase(tasks.begin());
+        energy -= 10;
+    } else {
+        std::cout << name << " is too tired or has no tasks.\n";
+    }
+}
+
+void Player::rest() {
+    energy = maxEnergy;
+    std::cout << name << " has rested and regained energy.\n";
+}
+
+int Player::getEnergy() const {
+    return energy;
+}
+
+std::string Player::getName() const {
+    return name;
+}
+
+void Player::displayStatus() const {
+    std::cout << "Player: " << name << ", Energy: " << energy << "/" << maxEnergy << "\n";
+    std::cout << "Tasks:\n";
+    for (const std::string& task : tasks) {
+        std::cout << "- " << task << "\n";
+    }
+}
--- Player.h
+++ Player.h
@@ -1,17 +1,29 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Player {
-    protected:
+protected:
+    std::string name;
+    int energy;
+    int maxEnergy;
+    std::vector<std::string> tasks;
 
-    public:
-        Player();
-        Player(int max);
+public:
+    Player();
+    Player(const std::string& name, int maxEnergy);
 
-        void assignTask();
+    void assignTask(const std::string& task);
+    void performTask();
+    void rest();
+
+    int getEnergy() const;
+    std::string getName() const;
+    void displayStatus() const;
 };
 
 #endif
